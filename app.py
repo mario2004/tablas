862f5d4506787ff5e011c37cbb016a5922f2fbc1
Mario <mario2004@gmail.com>
2024-11-29 13:35:08 +0100

    venga

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # O cualquier URI de base de datos SQL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tablas2.db'
 
 db = SQLAlchemy(app)
 
-# Clase Appointment
-class Appointment(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    requests = db.relationship('Request', backref='appointment', lazy='dynamic')
-    amount = db.Column(db.Float, nullable=False)
-    appointmentDate = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    place = db.Column(db.String(200), nullable=False)
-    currencyFrom = db.Column(db.String(10), nullable=False)
-    currencyTo = db.Column(db.String(10), nullable=False)
-    lat = db.Column(db.Float, nullable=False)
-    lon = db.Column(db.Float, nullable=False)
-    reserved=db.Column(db.Boolean, nullable=False, default=False)
+    name = db.Column(db.String)
+    comments = db.relationship('Comment')
 
-class Request(db.Model):
-    idRequest = db.Column(db.Integer, primary_key=True)
-    requestDate = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    requestText = db.Column(db.Text, unique=True, nullable=True)
-    appointment_id =  db.Column(db.Integer, db.ForeignKey('appointment.id'))
-
-def get_appointments():
-    citas = Appointment.query.all()
-    return citas
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    idUser = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/set')
+def set():
+    with app.app_context():
+        s = db.session()
+        user = User(name='Fulano')
+        s.add(Comment(text='comentario 1', idUser=1))
+        s.add(Comment(text='comentario 2', idUser=1))
+        s.add(Comment(text='comentario 3', idUser=1))
+        s.add(user)
+        s.commit()
+    return render_template('home.html')
+
+@app.route('/get')
+def get():
+    with app.app_context():
+        s = db.session()
+        usuario = s.query(User).filter(User.name == 'Fulano').one()
+        print ("employees: {}".format(usuario.comments))
+        print ("empleat zero: {}".format(usuario.comments[0].text))
+        for t in usuario.comments:
+            print ("{}".format(t.text))
     return render_template('home.html')
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-    citas = get_appointments()
-    for cita in citas:
-        print(cita.idAppointment)
